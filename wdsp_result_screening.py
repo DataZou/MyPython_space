ffb00933bafbf066ae5773abc4fb444ec859248e
DataZou <laurie.zouxd@gmail.com>
2015-03-13 09:12:38 +0800

    add a scripts:wdsp_result_screening.py

---
+++ wdsp_result_screening.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+'''
+Desc:This script screening wdsp results by the condition:
+Condition: if tetradNum > 0, score >= 48;else score >= 60
+Usage: python wdsp_result_screening.py wdsp_result.out
+Author:Xu-Dong Zou
+Time:2015-03-13
+'''
+import sys
+
+parameter = {}
+fhi = open("parameters.txt")
+for line in fhi:
+	line = line.strip()
+	if "blade num" in line:
+		break
+for line in fhi:
+	if "=" in line:
+		line = line.strip()
+		words = line.split()
+		parameter[words[0]] = float(words[2])
+	else:
+		break
+fhi.close()
+
+tetrad_num = {}
+tetrad_score = {}
+output = {}
+entry = ''
+ave_score = 0.0
+repNum = 0
+fhi = open(sys.argv[1])
+fho = open("WD40s_6_48.out",'w')
+for line in fhi.readlines():
+	words = line.split()
+	if line[0]==">":
+		if len(words)==4:
+			entry = words[1]
+			ave_score = float(words[2])
+			repNum = int(words[3])
+			tetrad_score[entry] = 0.0
+			tetrad_num[entry] = 0
+			output[entry] = ''
+	elif len(words)>3:
+		output[entry] += line
+		tetrad_score[entry] += float(words[-1])
+		if float(words[-1]) >= 28.0:
+			tetrad_num[entry] += 1
+	else:
+		if ave_score > 0 and repNum >= 6:
+			score = ave_score - tetrad_score[entry] - parameter[str(repNum)]
+			if tetrad_num > 0:
+				if score/repNum >= 48:
+					print >>fho,"> %s %.2f %d" % (entry,ave_score,repNum)
+					print >>fho,output[entry]
+					entry = ''
+					ave_score = 0.0
+					repNum = 0
+			else:
+				if score/repNum >= 60:
+					print >>fho,"> %s %.2f %d" % (entry,ave_score,repNum)
+					print >>fho,output[entry]
+					entry = ''
+					ave_score = 0.0
+					repNum = 0
+fhi.close()
+fho.close()
